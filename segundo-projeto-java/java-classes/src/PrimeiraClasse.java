c532726ac3ad7e49825d76f59a24a203e27fa665
Daniel Cardoso <danielcardoso1974@hotmail.com>
2022-11-01 19:51:02 -0300

    primeiro commit

---
+++ segundo-projeto-java/java-classes/src/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+public class PrimeiraClasse {
+
+    public static void main (String[] args){
+        // bloco de codigo de impressão 
+        System.out.print("Olá Mundo!");
+        System.out.print("\nBem vindo ao curso de Java...");
+
+    }
+    
+}
