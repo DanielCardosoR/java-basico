d8914c9595525fff693e5bfeff770a75c5c863b4
Daniel Cardoso <danielcardoso1974@hotmail.com>
2022-11-13 16:36:14 -0300

    Exemplo de uma pequena calculadora

--- segundo-projeto-java/java-classes/src/edu/dnl/segundasemana/variaveis.java
+++ segundo-projeto-java/java-classes/src/edu/dnl/segundasemana/variaveis.java
@@ -1,13 +1,33 @@
 package edu.dnl.segundasemana;
 
-
+//Exemplo de uma pequena calculadora 
+//13-11-2022
 
 public class variaveis {
 
-    public static void main(String[] args) {
-        
+
+    //metodo publico que realiza a soma.
+
+    public static int somar(int numero1,int numero2){
+        return numero1 + numero2;
+
+    }
+    //metodo publico que realiza a subtração. 
+    public static int subtrair( int numero1,int numero2) {
+        return numero1 - numero2;
         
     }
-
     
+    public static void main(String[] args) {
+        
+        int numero1 = 10;
+        int numero2 = 5;
+        //chamada do metodo somar e o metodo subtrair 
+        // retorno as variaveis resultadoSoma e resultadoSubtração.
+        int resultadoSoma = somar(numero1, numero2);
+        int resultadoSubtracao = subtrair(numero1, numero2);
+
+        System.out.println("Soma:"+resultadoSoma);
+        System.out.println("Subtração:"+resultadoSubtracao);
+     }
 }
