6ead683a0fb9c6f1d09139de51ef12760b7ae3cc
Daniel Cardoso <danielcardoso1974@hotmail.com>
2022-11-16 18:59:15 -0300

    calculadora java

---
+++ segundo-projeto-java/java-classes/src/edu/dnl/terceirasemana/calculadora.java
@@ -0,0 +1,15 @@
+package edu.dnl.terceirasemana;
+/**
+ * @author Daniel 
+ * @version 1.0
+ * @since 16/11/2022
+ * 
+ */
+
+public class calculadora {
+
+    public int somar(int numero1 ,int numero2){
+        return numero1 + numero2;
+    }
+
+}
