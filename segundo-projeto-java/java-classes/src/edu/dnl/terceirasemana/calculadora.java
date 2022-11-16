c40bcfd8e804a3cf5d5aad77b490728db56b3478
Daniel Cardoso <danielcardoso1974@hotmail.com>
2022-11-16 19:25:12 -0300

    atualizando o projeto

--- segundo-projeto-java/java-classes/src/edu/dnl/terceirasemana/calculadora.java
+++ segundo-projeto-java/java-classes/src/edu/dnl/terceirasemana/calculadora.java
@@ -6,10 +6,35 @@ package edu.dnl.terceirasemana;
  * 
  */
 
+/**
+ * Pequena calculadora 
+ * realiza a soma e subtração
+ * 
+ */
+
 public class calculadora {
 
-    public int somar(int numero1 ,int numero2){
+    //metodo publico que realiza a soma.
+    public static int somar(int numero1 ,int numero2){
         return numero1 + numero2;
     }
 
+      //metodo publico que realiza a subtração. 
+      public static int subtrair( int numero1,int numero2) {
+        return numero1 - numero2;
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        int numero1 = 10;
+        int numero2 = 5;
+        //chamada do metodo somar e o metodo subtrair 
+        // retorno as variaveis resultadoSoma e resultadoSubtração.
+        int resultadoSoma = somar(numero1, numero2);
+        int resultadoSubtracao = subtrair(numero1, numero2);
+
+        System.out.println("Soma:"+resultadoSoma);
+        System.out.println("Subtração:"+resultadoSubtracao);
+     }
 }
